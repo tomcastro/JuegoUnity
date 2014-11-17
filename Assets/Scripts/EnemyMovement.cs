b71307ddaa3d90068aeb858dfa80e5786625da99
Tomás Castro <tcastrog10@gmail.com>
2014-11-17 18:16:14 -0400

    Player y esqueleto. El esqueleto ya te sigue.

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour 
+{
+	Transform player;
+	NavMeshAgent nav;
+
+	void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		nav = GetComponent <NavMeshAgent> ();
+		Debug.Log (nav.acceleration);
+	}
+
+	void Update()
+	{
+		nav.SetDestination (player.position);
+	}
+}
